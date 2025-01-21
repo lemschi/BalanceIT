04be57b992cdeb9b41981f0bbc310b67dd7dd361
Sqhodela <goessler.simon@hak-liezen.at>
2025-01-21 20:00:26 +0100

    Update PlatformScript.cs

--- BalanceITUnityGame/Assets/BallGame/Scripts/PlatformScript.cs
+++ BalanceITUnityGame/Assets/BallGame/Scripts/PlatformScript.cs
@@ -1,7 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEditor;
-using UnityEditor.Presets;
 using UnityEngine;
 
 public class PlatformScript : MonoBehaviour
@@ -9,7 +8,7 @@ public class PlatformScript : MonoBehaviour
     private GameObject plain;
     private GameObject plain2;
 
-    public DefaultPreset Platform;
+    public UnityEditor.Presets.DefaultPreset Platform;
     // Start is called before the first frame update
     void Start()
     {
