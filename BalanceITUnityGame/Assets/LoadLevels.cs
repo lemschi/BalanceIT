46545e63dd5acee58e132b7a9f9b22561d04e920
Sqhodela <goessler.simon@hak-liezen.at>
2025-04-22 18:17:15 +0200

    ui fix

--- BalanceITUnityGame/Assets/LoadLevels.cs
+++ BalanceITUnityGame/Assets/LoadLevels.cs
@@ -7,16 +7,28 @@ public class LoadLevels : MonoBehaviour
 {
     public void Anybutton ()
     {
+        Debug.Log("Load Main Menu");
+        SceneManager.UnloadSceneAsync(SceneManager.GetActiveScene());
         SceneManager.LoadScene(1);
     }
 
     public void LoadSelectmount ()
     {
+        SceneManager.UnloadSceneAsync(SceneManager.GetActiveScene());
         SceneManager.LoadScene(2);
     }
 
     public void LoadSeilGame ()
     {
+        Debug.Log("Load Main SeilGame");
+        SceneManager.UnloadSceneAsync(SceneManager.GetActiveScene());
         SceneManager.LoadScene(3);
     }
+    public void LoadBallGame()
+    {
+        Debug.Log("Load Main BallGame");
+        SceneManager.UnloadSceneAsync(SceneManager.GetActiveScene());
+        SceneManager.LoadScene(4);
+    }
+
 }
